24fc1ab5665f7acd7f3e3986b4a35ba22ab926dd
Nimit Vijayvargee <nimit.vijayvargee@gmail.com>
2025-01-13 17:25:28 +0530

    Reset data and resuming work on ancient forge

--- src/main/java/net/voidforged/Voidforged.java
+++ src/main/java/net/voidforged/Voidforged.java
@@ -2,16 +2,8 @@ package net.voidforged;
 
 import net.fabricmc.api.ModInitializer;
 
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.resource.featuretoggle.FeatureSet;
-import net.minecraft.screen.ScreenHandlerType;
-import net.minecraft.util.Identifier;
-import net.voidforged.block.ModBlockEntities;
 import net.voidforged.block.ModBlocks;
 import net.voidforged.item.ModItems;
-import net.voidforged.screenhandler.AncientForgeScreenHandler;
-import net.voidforged.screenhandler.ModScreenHandlers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -25,8 +17,8 @@ public class Voidforged implements ModInitializer {
 	public void onInitialize() {
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
-		ModBlockEntities.load();
-		ModScreenHandlers.load();
-		ModScreenHandlers.load();
+		//ModBlockEntities.load();
+		//ModScreenHandlers.load();
+		//ModScreenHandlers.load();
 	}
 }
